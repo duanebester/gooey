d6a648d9dd1db5d3568d1cf008e6a02bde91e08c
Duane Bester <bester.duane@gmail.com>
2026-01-10 11:08:57 -0600

    fix build

--- charts/build.zig
+++ charts/build.zig
@@ -4,28 +4,17 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // Get gooey dependency from parent
-    const gooey_dep = b.dependency("gooey", .{
-        .target = target,
-        .optimize = optimize,
-    });
-
     // Create the gooey-charts module
-    const charts_mod = b.addModule("gooey-charts", .{
+    // Note: When used as a dependency, the parent build.zig should provide
+    // the "gooey" import via addImport() after getting this module.
+    _ = b.addModule("gooey-charts", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
-        .imports = &.{
-            .{ .name = "gooey", .module = gooey_dep.module("gooey") },
-        },
     });
 
-    // Tests - use root_module API for Zig 0.15
-    const tests = b.addTest(.{
-        .root_module = charts_mod,
-    });
-
-    const run_tests = b.addRunArtifact(tests);
-    const test_step = b.step("test", "Run gooey-charts tests");
-    test_step.dependOn(&run_tests.step);
+    // Tests are run from the parent build.zig which provides the gooey module.
+    // Standalone testing is not supported since this package depends on gooey.
+    const test_step = b.step("test", "Run gooey-charts tests (requires parent build)");
+    _ = test_step;
 }
